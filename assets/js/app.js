e0274d935c857a15aa1d85919ad29538274b9224
jbourgin <jeremy.bourgin@etu.umontpellier.fr>
2019-11-08 00:49:27 +0100

    script css include + test

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-const $ = require('jquery');
-
-require('bootstrap');
-
-// any CSS you require will output into a single css file (app.css in this case)
-//require('../css/app.css');
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Need Bootstrap? Install it with "yarn add bootstrap", then uncomment to require it.
+require('bootstrap');
+
+// import your codes
+require("./test.js");
